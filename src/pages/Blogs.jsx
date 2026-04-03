6a60fb9cead9c300e516f5deca30e93c318386fc
gloria-byte-mc <gloriamachoka1@gmail.com>
2026-04-03 15:39:48 +0300

    style

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,21 +1,3 @@
-{/* 
-
-import React from 'react'
-import '../styles/blogs.css'
-import Navbar from '../components/Navbar'
-
-const Blogs = () => {
-  return (
-    <>
-
-    <Navbar solid />
-    
-    </>
-  )
-}
-
-export default Blogs
-*/}
 
 
 import React, { useState } from 'react'
